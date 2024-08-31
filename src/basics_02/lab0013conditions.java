ae8ef58a3aa149c1110a7c176bcea43a6b101e33
patilajASND <patilomkar1025@gmail.com>
2024-08-31 12:46:36 +0530

    first

--- src/basics_02/lab0013conditions.java
+++ src/basics_02/lab0013conditions.java
@@ -1,7 +1,26 @@
 package src.basics_02;
 
+import java.util.Scanner;
+
 public class lab0013conditions {
     public static void main(String[] args) {
+//// if else
+        // if else if
+        // Grade Calculator
+
+        Scanner sc = new Scanner(System.in);
+        int Score = sc.nextInt();
+        if (Score >= 90 && Score <= 100) {
+            System.out.println("You Scored 'A'");
+        } else if (Score >= 80 && Score <= 89) {
+            System.out.println("You Scored 'B'");
+        } else if (Score >= 70 && Score <= 79) {
+            System.out.println("You Scored 'C'");
+        } else if (Score >= 60 && Score <= 69) {
+            System.out.println("You Scored 'D'");
+        } else {
+            System.out.println("You have Failed the Exam");
+        }
 
     }
 }
