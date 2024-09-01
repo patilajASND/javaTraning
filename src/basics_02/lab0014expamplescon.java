a54627e09bc4cbb3ec6a2c71cdff0cadca6a2909
patilajASND <patilomkar1025@gmail.com>
2024-09-01 12:46:11 +0530

    first

--- src/basics_02/lab0014expamplescon.java
+++ src/basics_02/lab0014expamplescon.java
@@ -1,2 +1,21 @@
-package src.basics_02;public class lab0014expamplescon {
+package src.basics_02;
+
+import java.util.Scanner;
+
+public class lab0014expamplescon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter your mesurment");
+        int side1=sc.nextInt();
+        int side2=sc.nextInt();
+        int side3=sc.nextInt();
+
+        if ((side1==side2)&& (side2==side3)&&(side1==side3)){
+            System.out.println("it is equlateral Trangle");
+        } else if ((side1==side2)||(side2==side3)||(side1==side3)){
+            System.out.println("it is a isoscles");
+        }else {
+            System.out.println("scalne");
+        }
+    }
 }
