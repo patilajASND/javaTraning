ea38d393f0f22e456ec69565f97f7d3d1efc3c34
patilajASND <patilomkar1025@gmail.com>
2024-09-10 23:20:05 +0530

    first

---
+++ src/basics/examples/lab0010.java
@@ -0,0 +1,15 @@
+package src.basics.examples;
+
+public class lab0010 {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            if(i%2==0){
+                System.out.println(i+" it is a even Number");
+            } else if (i%2 != 0){
+                System.out.println( i +" it is a odd Number");
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
---
+++ src/basics_02/lab0016fordoloop.java
@@ -0,0 +1,8 @@
+package src.basics_02;
+
+public class lab0016fordoloop {
+    public static void main(String[] args) {
+
+    }
+
+}
